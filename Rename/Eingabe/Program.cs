34b6789f0c1236e4c2baf2afe7a76766b1007f3e
LotAnderson <86662048+LotAnderson@users.noreply.github.com>
2025-07-10 12:11:03 +0200

    Eingabe kopiert

---
+++ Rename/Eingabe/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eingabe {
+    class Program {
+        static void Main(string[] args) {
+            if (args.Length==0)
+                Console.WriteLine("Bitte Parameter eingeben!");
+            for (int i = 0; i < args.Length; i++) {
+                Console.WriteLine($"Eingabe {i}:{args[i]}");
+            }
+        }
+    }
+}
